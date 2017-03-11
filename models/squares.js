880abc5476e6b5d2aab97f80095c35851d4083b7
williebabzan <jakobwilliamaberg@gmail.com>
2017-03-11 20:07:59 +0100

    1km squares added to database

--- models/squares.js
+++ models/squares.js
@@ -1,5 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Squares", {
-    name: DataTypes.STRING
+    population: DataTypes.INTEGER,
+    c1Lat: DataTypes.STRING,
+    c1Lon: DataTypes.STRING,
+    c2Lat: DataTypes.STRING,
+    c2Lon: DataTypes.STRING, 
+    c3Lat: DataTypes.STRING,
+    c3Lon: DataTypes.STRING, 
+    c4Lat: DataTypes.STRING,
+    c4Lon: DataTypes.STRING,
+    cMidLat: DataTypes.STRING,
+    cMidLon: DataTypes.STRING
   })
 }
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var db      = require('./models');
 
 var http = require('http');
 var port = process.env.PORT || 8000;
+var utm = require('utm');
+const fs = require('fs'); 
 
 
 var counter = 0;
@@ -29,8 +31,47 @@ db.sequelize.sync().then(function() {
       })
     } else if (path == "/importsquares") {
 
+        let obj, pop;  
+        let easting, northing, LatLon, latitude, longitude;
 
-      // JAKOBS CODE HERE
+        fs.readFile('./density.json', 'utf8', (err, jsonData) => {
+          if (err) throw err;
+
+          obj = JSON.parse(jsonData);
+
+          var size = Object.keys(obj.features).length;
+
+          for (let i = 0; i < size; i++) {
+            if(obj.features[i].geometry!=null){
+              let pop = parseInt(obj.features[i].properties.Pop);
+              
+              console.log(pop);
+              //console.log(obj.features[0].geometry.coordinates);*/
+
+              let coords = obj.features[i].geometry.coordinates[0].length - 1;
+
+              for(let j = 0; j < coords; j++ ){
+
+                easting = obj.features[i].geometry.coordinates[0][j][0];
+                northing = obj.features[i].geometry.coordinates[0][j][1];
+
+                //console.log(easting + " " + northing); 
+                LatLon = utm.toLatLon(easting, northing, 33, '', true, true);
+
+                latitude += LatLon.latitude;
+                longitude += LatLon.longitude;  
+              }
+
+              midLat = latitude / coords; 
+              midLong = longitude / coords;
+
+              latitude = 0;
+              longitude = 0;
+
+            }  
+          }
+
+        });
 
 
     }
