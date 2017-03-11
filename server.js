556b32ac60e1e1fc2ed8e2c9741a6e301b4204f8
Viktor Gustafsson <viktor@vigu.se>
2017-03-11 20:11:21 +0100

    Fixed FS bug

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 // Send plain text headers and 'Hello World' to each client
 
 var db      = require('./models');
-const fs      = require('fs');
 const readline = require('readline');
 
 var http = require('http');
 var port = process.env.PORT || 8000;
 var utm = require('utm');
-const fs = require('fs'); 
+const fs = require('fs');
 
 
 var counter = 0;
@@ -57,7 +56,7 @@ db.sequelize.sync().then(function() {
       })
     } else if (path == "/importsquares") {
 
-        let obj, pop;  
+        let obj, pop;
         let easting, northing, LatLon, latitude, longitude;
 
         fs.readFile('./density.json', 'utf8', (err, jsonData) => {
@@ -70,7 +69,7 @@ db.sequelize.sync().then(function() {
           for (let i = 0; i < size; i++) {
             if(obj.features[i].geometry!=null){
               let pop = parseInt(obj.features[i].properties.Pop);
-              
+
               console.log(pop);
               //console.log(obj.features[0].geometry.coordinates);*/
 
@@ -81,20 +80,20 @@ db.sequelize.sync().then(function() {
                 easting = obj.features[i].geometry.coordinates[0][j][0];
                 northing = obj.features[i].geometry.coordinates[0][j][1];
 
-                //console.log(easting + " " + northing); 
+                //console.log(easting + " " + northing);
                 LatLon = utm.toLatLon(easting, northing, 33, '', true, true);
 
                 latitude += LatLon.latitude;
-                longitude += LatLon.longitude;  
+                longitude += LatLon.longitude;
               }
 
-              midLat = latitude / coords; 
+              midLat = latitude / coords;
               midLong = longitude / coords;
 
               latitude = 0;
               longitude = 0;
 
-            }  
+            }
           }
 
         });
